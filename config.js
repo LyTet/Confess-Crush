66e121df1fee8ae8fdac9440b6460884447ec86f
LyTet <106630901+LyTet@users.noreply.github.com>
2022-05-31 22:28:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    titleWeb: "Manhhhh",
-    introTitle: 'chào mày',
-    introDesc: `tao có chuyện muốn hỏi 
+    titleWeb: "Tet1",
+    introTitle: 'Chào các thành viên NABT',
+    introDesc: `Mình có 1 câu hỏi rất quan trọng cho các bạn 
     ...`,
-    btnIntro: '^^ấn vô đây xong tao hỏi ^^',
-    title: 'mày là con gái phải không ?',
+    btnIntro: 'Các bạn ấn vào đây nhé',
+    title: 'Các bạn có yêu quý NABT không ?',
     desc: 'trả lời thật lòng đi ',
-    btnYes: 'đúng rồi tao là con gái :33',
-    btnNo: 'đéo nha :3',
-    question: 'tại sao mày lại là con gái hhu... <3',
-    btnReply: 'Gửi cho cho Manh i nè  <3',
-    reply: 'bởi vì đấy là ước muốn của tao từ lâu rồi <33333333',
-    mess: 'tao biết ngay mà haizz',
-    messDesc: 'tao sẽ giữ bí mật cho mày  .',
-    btnAccept: 'ấn vô đây sẽ ra nick fb ngừi iu mày <3',
-    messLink: 'https://www.facebook.com/tlinh.gei' 
+    btnYes: 'Mình rất yêu quý Boss và Box NABT',
+    btnNo: 'Rất là ghét',
+    question: 'Tại sao lại yêu quý NABT',
+    btnReply: 'Vì NATB rất nhiều trai xinh gái đẹp',
+    reply: 'Cùng nhau đồng hành cùng NABT ở box và discord nhé',
+    mess: 'Rất là ok ạ',
+    messDesc: 'NABT yêu các bạn.',
+    btnAccept: 'ấn vô đây sẽ ra facebook 1 bạn xinh gái tạo ra code này',
+    messLink: 'https://www.facebook.com/ssdieuly/' 
 }
